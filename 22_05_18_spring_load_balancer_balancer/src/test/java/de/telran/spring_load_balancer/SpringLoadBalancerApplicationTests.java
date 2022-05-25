ebc0606af4f96c90bd53b981c8ab3f9006ddfaf5
Nataton <nataton02@gmail.com>
2022-05-25 08:41:52 +0200

    add spring load_balancer balancer

---
+++ 22_05_18_spring_load_balancer_balancer/src/test/java/de/telran/spring_load_balancer/SpringLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.spring_load_balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
