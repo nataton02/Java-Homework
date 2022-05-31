f216832ece3c358ae54b08901dbdffe506ff88b0
Nataton <nataton02@gmail.com>
2022-05-31 22:02:12 +0200

    add spring persons

--- 22_05_31_spring_persons/src/main/java/de/telran/persons/controller/PersonController.java
+++ 22_05_31_spring_persons/src/main/java/de/telran/persons/controller/PersonController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+// 1. Создать метод, который будет обрабатывать эндпоинт GET /persons. В этом методе создать
+// лист персонов и передать этот лист в шаблон под названием "persons.html"
+// 2. Написать шаблон persons.html, используя thymeleaf синтаксис, который берет из модели
+// лист персонов и распечатывает их в темплейте, используя таги для списка <ul> и <li>
+// (зайти на сайт thymeleaf и посмотреть синтаксис для перебора коллекции)
+
 @Controller
 public class PersonController {
 
